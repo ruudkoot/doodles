c4ae9041a9cedec157fc0a15307aa4308a20ca85
Ruud Koot <git@ruudkoot.nl>
2012-09-19 15:05:04 +0200

    call-by-value

--- IncrementalExceptionAnalysis/Main.hs
+++ IncrementalExceptionAnalysis/Main.hs
@@ -119,7 +119,7 @@ instance Fresh Ty where
                return (TyVar a)
     
 instance LaTeX Ty where
-    latex (TyVar a     ) = "\\phi" ++ a
+    latex (TyVar a     ) = "\\tau" ++ a
     latex (TyCon TyBool) = "\\mathbf{Bool}"
     latex (TyCon TyInt ) = "\\mathbf{Int}"
     latex (TyFun t1 t2 ) = "(" ++ latex t1 ++ "\\rightarrow" ++ latex t2 ++ ")"
@@ -187,7 +187,8 @@ instance Substitute TyEnv where
 
 infer :: TyEnv -> Expr -> State [Ident] (Ty, Subst)
 infer env (Var x)
-    = return (fromJust (M.lookup x env), idSubst)
+    | Just t <- M.lookup x env = return (t, idSubst)
+    | otherwise                = error "variable not in scope"
 infer env (Con c)
     = case c of
         Bool _ -> return (TyCon TyBool, idSubst)
@@ -212,6 +213,8 @@ infer env Crash
          
 -- | Analysis
 
+-- * Effects
+
 data EffCon
     = EffNone
     | EffCrash
@@ -235,12 +238,14 @@ instance Fresh Eff where
                return (EffUnif a)
 
 instance LaTeX Eff where
-    latex (EffUnif u) = "\\phi" ++ u
+    latex (EffUnif u) = "\\varphi" ++ u
     latex (EffCon  c) = latex c
     
 effcon :: Eff -> EffCon
 effcon (EffCon c) = c
 effcon _          = error "effect not a constant"
+
+-- * Annotated types (call-by-value)
 
 data AnnTy
     = AnnTyVar Ident
@@ -251,6 +256,11 @@ data AnnTy
 instance Fresh AnnTy where
     fresh = do a <- fresh
                return (AnnTyVar a)
+               
+instance FreeVars AnnTy where
+    ftv (AnnTyCon _     ) = S.empty
+    ftv (AnnTyFun t _ t') = ftv t `S.union` ftv t'
+    ftv (AnnTyVar a     ) = S.singleton a
     
 instance LaTeX AnnTy where
     latex (AnnTyVar a        ) = "\\widehat\\tau" ++ a
@@ -258,21 +268,11 @@ instance LaTeX AnnTy where
     latex (AnnTyCon TyInt    ) = "\\mathbf{Int}"
     latex (AnnTyFun t1 eff t2) = "(" ++ latex t1 ++ "\\overset{" ++ latex eff ++ "}{\\rightarrow}" ++ latex t2 ++ ")"
     
--- * Environments
+-- * Environments (call-by-value)
 
 type AnnTyEnv = M.Map Ident AnnTy
 
-instance AnnSubstitute AnnTyEnv where
-    subst $$@ env = M.map (subst $$@) env
-
--- * Free variables
-
-instance FreeVars AnnTy where
-    ftv (AnnTyCon _     ) = S.empty
-    ftv (AnnTyFun t _ t') = ftv t `S.union` ftv t'
-    ftv (AnnTyVar a     ) = S.singleton a
-
--- * Substitutions
+-- * Substitutions (call-by-value)
 
 data AnnSubst = AnnSubst (M.Map Ident AnnTy) (M.Map Ident Eff)
 
@@ -300,13 +300,16 @@ instance AnnSubstitute AnnTy where
     _             $$@ x
         = x
 
+instance AnnSubstitute AnnTyEnv where
+    subst $$@ env = M.map (subst $$@) env
+
 instance AnnSubstitute Eff where
     AnnSubst _ ev $$@ (EffUnif u)
         | Just e <- M.lookup u ev = e
     _             $$@ x
         = x
 
--- * Unification
+-- * Unification (call-by-value)
 
 unify' :: AnnTy -> AnnTy -> AnnSubst
 unify' (AnnTyCon c1) (AnnTyCon c2)
@@ -344,7 +347,8 @@ unify'' _ _
 
 analyzeCBV :: AnnTyEnv -> Expr -> State [Ident] (AnnTy, EffCon, AnnSubst)
 analyzeCBV env (Var x)
-    = return (fromJust (M.lookup x env), EffNone, idAnnSubst)
+    | Just t <- M.lookup x env = return (t, EffNone, idAnnSubst)
+    | otherwise                = error "variable not in scope"
 analyzeCBV env (Con c)
     = case c of
         Bool _ -> return (AnnTyCon TyBool, EffNone, idAnnSubst)
@@ -363,10 +367,138 @@ analyzeCBV env (App e1 e2)
 analyzeCBV env (Let x e1 e2)
     = do (t1, eff1, subst1) <- analyzeCBV env e1
          (t2, eff2, subst2) <- analyzeCBV (M.insert x t1 (subst1 $$@ env)) e2
-         return (t2, eff1 `join` eff2, subst2 $$. subst1)
+         return (t2, {- subst2 $@@ -} eff1 `join` eff2, subst2 $$. subst1)
 analyzeCBV env Crash
     = do a <- fresh
          return (a, EffCrash, idAnnSubst)
+
+-- * Annotated types (call-by-name)
+
+data LazyAnnTy
+    = LazyAnnTyVar Ident
+    | LazyAnnTyCon TyCon
+    | LazyAnnTyFun LazyAnnTy Eff LazyAnnTy Eff
+    deriving (Eq, Ord, Show)
+    
+instance Fresh LazyAnnTy where
+    fresh = do a <- fresh
+               return (LazyAnnTyVar a)
+               
+instance FreeVars LazyAnnTy where
+    ftv (LazyAnnTyCon _       ) = S.empty
+    ftv (LazyAnnTyFun t _ t' _) = ftv t `S.union` ftv t'
+    ftv (LazyAnnTyVar a       ) = S.singleton a
+    
+instance LaTeX LazyAnnTy where
+    latex (LazyAnnTyVar a              ) = "\\widehat\\tau" ++ a
+    latex (LazyAnnTyCon TyBool         ) = "\\mathbf{Bool}"
+    latex (LazyAnnTyCon TyInt          ) = "\\mathbf{Int}"
+    latex (LazyAnnTyFun t1 eff1 t2 eff2) = "(" ++ latex t1 ++ "@" ++ latex eff1 ++ "\\rightarrow" ++ latex t2 ++ "@" ++ latex eff2 ++ ")"
+    
+-- * Environments (call-by-name)
+
+type LazyAnnTyEnv = M.Map Ident (LazyAnnTy, Eff)
+
+-- * Substitutions (call-by-name)
+
+data LazyAnnSubst = LazyAnnSubst (M.Map Ident LazyAnnTy) (M.Map Ident Eff)
+
+idLazyAnnSubst :: LazyAnnSubst
+idLazyAnnSubst = LazyAnnSubst M.empty M.empty
+
+($$$.) :: LazyAnnSubst -> LazyAnnSubst -> LazyAnnSubst
+s2 $$$. s1 = (s2 $$$@ s1) `substUnion` s2
+    where 
+        substUnion (LazyAnnSubst tv1 ev1) (LazyAnnSubst tv2 ev2)
+            = LazyAnnSubst (M.unionWith (error "type variables not distinct") tv1 tv2)
+                           (M.unionWith (error "effect variables not distinct") ev1 ev2)
+                    
+class LazyAnnSubstitute t where
+    ($$$@) :: LazyAnnSubst -> t -> t
+    
+instance LazyAnnSubstitute LazyAnnSubst where
+    subst $$$@ (LazyAnnSubst tv ev) = LazyAnnSubst (M.map (subst $$$@) tv) (M.map (subst $$$@) ev)
+
+instance LazyAnnSubstitute LazyAnnTy where
+    LazyAnnSubst tv _ $$$@ (LazyAnnTyVar a)
+        | Just t <- M.lookup a tv = t
+    subst             $$$@ (LazyAnnTyFun t eff t' eff')
+        = LazyAnnTyFun (subst $$$@ t) (subst $$$@ eff) (subst $$$@ t') (subst $$$@ eff')
+    _                 $$$@ x
+        = x
+
+instance LazyAnnSubstitute LazyAnnTyEnv where
+    subst $$$@ env = M.map (\(t, eff) -> (subst $$$@ t, subst $$$@ eff)) env
+
+instance LazyAnnSubstitute Eff where
+    LazyAnnSubst _ ev $$$@ (EffUnif u)
+        | Just e <- M.lookup u ev = e
+    _             $$$@ x
+        = x
+
+-- * Unification (call-by-name)
+
+unify_ :: LazyAnnTy -> LazyAnnTy -> LazyAnnSubst
+unify_ (LazyAnnTyCon c1) (LazyAnnTyCon c2)
+    | c1 == c2 = idLazyAnnSubst
+unify_ (LazyAnnTyVar a) (LazyAnnTyVar a')
+    = LazyAnnSubst (M.singleton a (LazyAnnTyVar a')) M.empty
+unify_ (LazyAnnTyVar a) t
+    | a `S.member` ftv t = error "occurs check"
+    | otherwise          = LazyAnnSubst (M.singleton a t) M.empty
+unify_ t (LazyAnnTyVar a)
+    | a `S.member` ftv t = error "occurs check"
+    | otherwise          = LazyAnnSubst (M.singleton a t) M.empty
+unify_ (LazyAnnTyFun t1 eff1 t2 eff2) (LazyAnnTyFun t'1 eff'1 t'2 eff'2)
+    = let subst1 = unify_ t1 t'1
+          subst2 = unify_ (subst1 $$$@ t2) (subst1 $$$@ t'2)
+          subst3 = unify__ eff1 eff'1
+          subst4 = unify__ eff2 eff'2
+       in subst4 $$$. subst3 $$$. subst2 $$$. subst1
+unify_ _ _
+    = error "cannot unify types"
+
+unify__ :: Eff -> Eff -> LazyAnnSubst
+unify__ (EffUnif u1) (EffUnif u2)
+    = LazyAnnSubst M.empty (M.singleton u1 (EffUnif u2))
+unify__ (EffUnif u) e
+    = LazyAnnSubst M.empty (M.singleton u e)
+unify__ e (EffUnif u)
+    = LazyAnnSubst M.empty (M.singleton u e)
+unify__ (EffCon c1) (EffCon c2)
+    | c1 == c2 = idLazyAnnSubst
+unify__ _ _
+    = error "cannot unify effects"
+
+-- * Inference (call-by-name)
+
+analyzeCBN :: LazyAnnTyEnv -> Expr -> State [Ident] (LazyAnnTy, EffCon, LazyAnnSubst)
+analyzeCBN env (Var x)
+    | Just (t, eff) <- M.lookup x env = return (t, effcon eff, idLazyAnnSubst)
+    | otherwise                       = error "variable not in scope"
+analyzeCBN env (Con c)
+    = case c of
+        Bool _ -> return (LazyAnnTyCon TyBool, EffNone, idLazyAnnSubst)
+        Int  _ -> return (LazyAnnTyCon TyInt, EffNone, idLazyAnnSubst)
+analyzeCBN env (Abs x e0)
+    = do a <- fresh
+         u <- fresh
+         (t0, eff0, subst0) <- analyzeCBN (M.insert x (a, u) env) e0
+         return (LazyAnnTyFun (subst0 $$$@ a) (subst0 $$$@ u) t0 (EffCon eff0), EffNone, subst0)
+analyzeCBN env (App e1 e2)
+    = do (t1, eff1, subst1) <- analyzeCBN env e1
+         (t2, eff2, subst2) <- analyzeCBN (subst1 $$$@ env) e2
+         a <- fresh
+         u <- fresh
+         let subst3 = unify_ (subst2 $$$@ t1) (LazyAnnTyFun t2 (EffCon eff2) a u)
+         return (subst3 $$$@ a, effcon u `join` {- subst $@@ -} eff1, subst3 $$$. subst2 $$$. subst1)
+analyzeCBN env (Let x e1 e2)
+    = do (t1, eff1, subst1) <- analyzeCBN env e1
+         (t2, eff2, subst2) <- analyzeCBN (M.insert x (t1, EffCon eff1) (subst1 $$$@ env)) e2
+         return (t2, eff2, subst2 $$$. subst1)
+analyzeCBN env Crash
+    = do a <- fresh
+         return (a, EffCrash, idLazyAnnSubst)
     
 -- | Examples
 
