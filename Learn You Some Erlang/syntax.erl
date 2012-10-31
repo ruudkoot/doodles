24106721be0eb72312a0a18fec603c1adf633b4d
Ruud Koot <git@ruudkoot.nl>
2012-10-31 17:38:16 +0100

    some more erlang

---
+++ Learn You Some Erlang/syntax.erl
@@ -0,0 +1,11 @@
+-module(syntax).
+-export([greet/2]).
+
+greet(male, Name) ->
+    io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+    io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) ->
+    io:format("Hello, ~s!", [Name]).
+
+
--- Learn You Some Erlang/useless.erl
+++ Learn You Some Erlang/useless.erl
@@ -1,13 +1,17 @@
 -module(useless).
+-compile([debug_info, export_all]).
 -export([add/2, hello/0, greet_and_add_two/1]).
+-import(io, [format/1]).
+-define(sub(X,Y), X-Y).
 
 add(A,B) ->
     A + B.
 
 %% Shows greetings.
 hello() ->
-    io:format("Hello, world!~n").
+    format("Hello, world!~n").
     
 greet_and_add_two(X) ->
     hello(),
+    _ = ?sub(23,47),
     add(X,2).
