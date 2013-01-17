4b10364378728f40e067796d5b4ad40d351eac86
Ruud Koot <inbox@ruudkoot.nl>
2013-01-18 00:55:28 +0100

    sf

--- SoftwareFoundations/SfLib/Imp.v
+++ SoftwareFoundations/SfLib/Imp.v
@@ -437,3 +437,227 @@ Admitted. (* FIXME *)
 
 (** Syntax **)
 
+Inductive aexp : Type :=
+| ANum : nat -> aexp
+| AId : id -> aexp
+| APlus : aexp -> aexp -> aexp
+| AMinus : aexp -> aexp -> aexp
+| AMult : aexp -> aexp -> aexp.
+
+Definition X : id := Id 0.
+Definition Y : id := Id 1.
+Definition Z : id := Id 2.
+
+Inductive bexp : Type :=
+| BTrue : bexp
+| BFalse : bexp
+| BEq : aexp -> aexp -> bexp
+| BLe : aexp -> aexp -> bexp
+| BNot : bexp -> bexp
+| BAnd : bexp -> bexp -> bexp.
+
+(** Evaluation **)
+
+Fixpoint aeval (st : state) (e : aexp) : nat :=
+  match e with
+    | ANum n => n
+    | AId X => st X
+    | APlus a1 a2 => (aeval st a1) + (aeval st a2)
+    | AMinus a1 a2 => (aeval st a1) - (aeval st a2)
+    | AMult a1 a2 => (aeval st a1) * (aeval st a2)
+  end.
+
+Fixpoint beval (st : state) (e : bexp) : bool :=
+  match e with
+    | BTrue => true
+    | BFalse => false
+    | BEq a1 a2 => beq_nat (aeval st a1) (aeval st a2)
+    | BLe a1 a2 => ble_nat (aeval st a1) (aeval st a2)
+    | BNot b1 => negb (beval st b1)
+    | BAnd b1 b2 => andb (beval st b1) (beval st b2)
+  end.
+
+Example aexp1:
+  aeval (update empty_state X 5) (APlus (ANum 3) (AMult (AId X) (ANum 2))) = 13.
+Proof.
+  reflexivity.
+Qed.
+
+Example bexp1:
+  beval (update empty_state X 5) (BAnd BTrue (BNot (BLe (AId X) (ANum 4)))) = true.
+Proof.
+  reflexivity.
+Qed.
+
+(*** Commands ***)
+
+(** Syntax **)
+
+Inductive com : Type :=
+| CSkip : com
+| CAss : id -> aexp -> com
+| CSeq : com -> com -> com
+| CIf : bexp -> com -> com -> com
+| CWhile : bexp -> com -> com.
+
+Notation "'SKIP'" := CSkip.
+Notation "X '::=' a" := (CAss X a) (at level 60).
+Notation "c1 ; c2" := (CSeq c1 c2) (at level 80, right associativity).
+Notation "'WHILE' b 'DO' c 'END'" := (CWhile b c) (at level 80, right associativity).
+Notation "'IFB' e1 'THEN' e2 'ELSE' e3 'FI'" := (CIf e1 e2 e3)
+                                                  (at level 80, right associativity).
+
+Definition fact_in_coq : com :=
+  Z ::= AId X;
+  Y ::= ANum 1;
+  WHILE BNot (BEq (AId Z) (ANum 0)) DO
+        Y ::= AMult (AId Y) (AId Z);
+        Z ::= AMinus (AId Z) (ANum 1)
+  END.
+
+(** Exmaples **)
+
+Definition plus2 : com := X ::= (APlus (AId X) (ANum 2)).
+
+Definition XtimesYinZ : com := Z ::= (AMult (AId X) (AId Y)).
+
+Definition substract_slowly_body : com :=
+  Z ::= AMinus (AId Z) (ANum 1);
+  Z ::= AMinus (AId X) (ANum 1).
+
+Definition substract_slowly : com :=
+  WHILE BNot (BEq (AId X) (ANum 0)) DO
+        substract_slowly_body
+  END.
+
+Definition substract_3_from_5_slowly : com :=
+  X ::= ANum 3;
+  Z ::= ANum 5;
+  substract_slowly.
+
+Definition loop : com :=
+  WHILE BTrue DO
+    SKIP
+  END.
+
+Definition fact_body : com :=
+  Y ::= AMult (AId Y) (AId Z);
+  Z ::= AMinus (AId Z) (ANum 1).
+
+Definition fact_loop : com :=
+  WHILE BNot (BEq (AId Z) (ANum 0)) DO
+        fact_body
+  END.
+
+Definition fact_com : com :=
+  Z ::= AId X;
+  Y ::= ANum 1;
+  fact_loop.
+
+(*** Evaluation ***)
+
+(** Evaluation Function (Failed Attempt) **)
+
+Fixpoint ceval_fun_no_while (st : state) (c : com) : state :=
+  match c with
+    | SKIP => st
+    | l ::= a1 => update st l (aeval st a1)
+    | c1 ; c2 => let st' := ceval_fun_no_while st c1 in ceval_fun_no_while st' c2
+    | IFB b THEN c1 ELSE c2 FI => if (beval st b)
+                                  then ceval_fun_no_while st c1
+                                  else ceval_fun_no_while st c2
+    | WHILE b1 DO c1 END => if (beval st b1)
+                            then (* ceval_fun_no_while st (c1; WHILE b1 DO c1 END *) admit
+                            else st
+  end.
+
+(** Evaluation as a Relation **)
+
+Reserved Notation "c1 '/' st || st'" (at level 40, st at level 39).
+
+Inductive ceval : com -> state -> state -> Prop :=
+| E_Skip : forall st, SKIP / st || st
+| E_Ass : forall st a1 n X, aeval st a1 = n -> (X ::= a1) / st || (update st X n)
+| E_Seq : forall c1 c2 st st' st'',
+            c1 / st || st' -> c2 / st' || st'' -> (c1 ; c2) / st || st''
+| E_IfTrue : forall st st' b1 c1 c2,
+               beval st b1 = true -> c1 / st || st' ->
+               (IFB b1 THEN c1 ELSE c2 FI) / st || st'
+| E_IfFalse : forall st st' b1 c1 c2,
+                beval st b1 = false -> c2 / st || st' ->
+                (IFB b1 THEN c1 ELSE c2 FI) / st || st'
+| E_WhileEnd : forall st b1 c1,
+                 beval st b1 = false ->
+                 (WHILE b1 DO c1 END) / st || st
+| E_WhileLoop : forall st st' st'' b1 c1,
+                  beval st b1 = true -> c1 / st || st' ->
+                  (WHILE b1 DO c1 END) / st' || st'' ->
+                  (WHILE b1 DO c1 END) / st' || st''
+where "c1 '/' st '||' st'" := (ceval c1 st st').
+
+Example ceval_example1:
+  (X ::= ANum 2;
+   IFB BLe (AId X) (ANum 1)
+       THEN Y ::= ANum 3
+       ELSE Z ::= ANum 4
+   FI)
+  / empty_state
+  || (update (update empty_state X 2) Z 4).
+Proof.
+  apply E_Seq with (update empty_state X 2).
+    apply E_Ass. reflexivity.
+    apply E_IfFalse.
+      reflexivity.
+      apply E_Ass. reflexivity.
+Qed.
+
+Example ceval_example2:
+  (X ::= ANum 0; Y ::= ANum 1; Z ::= ANum 2)
+    / empty_state || (update (update (update empty_state X 0) Y 1) Z 2).
+Proof.
+  apply E_Seq with (update empty_state X 0).
+    apply E_Ass. reflexivity.
+    apply E_Seq with (update (update empty_state X 0) Y 1).
+      apply E_Ass. reflexivity.
+      apply E_Ass. reflexivity.
+Qed.
+
+Definition pup_to_n : com :=
+  Y ::= ANum 0;
+  WHILE BLe (ANum 0) (AId X) DO
+        Y ::= APlus (AId Y) (AId X);
+        X ::= AMinus (AId X) (ANum 1)
+  END.
+
+Example pup_to_2_ceval:
+  pup_to_n
+    / (update empty_state X 2) ||
+    update (update (update (update (update (update empty_state X 2) Y 0) Y 2) X 1) Y 3) X 0.
+Proof.
+  apply E_Seq with (update (update empty_state X 2) Y 0).
+    apply E_Ass. reflexivity.
+    apply E_WhileLoop with (update (update empty_state X 2) Y 0).
+      reflexivity.
+      apply E_Seq with (update (update (update empty_state X 2) Y 0) Y 2).
+        apply E_Ass. reflexivity.
+Admitted. (* FIXME *)
+
+
+(** Determinist of Evaluation **)
+
+Theorem ceval_deterministic:
+  forall c st st1 st2, c / st || st1 -> c / st || st2 -> st1 = st2.
+Proof.
+  intros c st st1 st2 E1 E2.
+  generalize dependent st2.
+  induction E1; intros st2 E2; inversion E2; subst.
+    reflexivity.
+    reflexivity.
+    assert (st' = st'0) as EQ1. apply IHE1_1; assumption.
+      subst st'0.
+      apply IHE1_2.
+      assumption.
+    
+      
+        
+        
